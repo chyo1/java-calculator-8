3ed9296c034420e3c2229a4ab163132cbd9e9d7f
chyo1 <hyowon3856@gmail.com>
2025-10-20 17:07:30 +0900

    feat: 문자열 덧셈 계산기 실행

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,45 @@
 package calculator;
 
 public class Calculator {
+    private IOController ioController = new IOController();
+    private Parser parser = new Parser();
+    private Operator operator = new Operator();
+    private Validator validator = new Validator();
+
+    private char[] basicSeperators = {',', ';'};
+    private Character customSeparator = null;
+
+    public void execute() {
+        // 입력 받기
+        String inputStr = ioController.input();
+
+        // 입력이 형식에 맞게 들어왔는지 확인
+        validator.validateInputEmpty(inputStr);
+        validator.validateBasicSeparator(inputStr, basicSeperators);
+
+        // 커스텀 구분자 확인
+        checkCustomSeparator(inputStr);
+
+        // 문자열 형식 오류 확인
+        validator.validateSeparatorType(inputStr, basicSeperators, customSeparator);
+
+        // 계산
+        String[] inputs = parser.getNumbers(inputStr, basicSeperators, customSeparator);
+        int total = 0;
+        for (String input : inputs) {
+            total = operator.sum(total, Integer.parseInt(input));
+        }
+
+        // 출력
+        ioController.print(total);
+    }
+
+    // 커스텀 구분자 추출
+    public void checkCustomSeparator(String s) {
+        if (s.startsWith("//")) {
+            validator.validateCustomSeparatorForm(s);
+        }
+        customSeparator = s.charAt(2);
+    }
 
 }
