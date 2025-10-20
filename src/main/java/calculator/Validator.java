7392232adc2746ec3faf203bec6f4a1bd403ba73
chyo1 <hyowon3856@gmail.com>
2025-10-20 16:58:44 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -1,11 +1,6 @@
 package calculator;
 
 public class Validator {
-    // 입력이 형식에 맞게 들어왔는지 확인
-    public void validateInputFormat(String s, char[] basicSeparators) {
-        validateInputEmpty(s);
-        validateSeparator(s, basicSeparators);
-    }
 
     // 입력이 없을 때 (빈 문자열, null)
     public void validateInputEmpty(String s) {
