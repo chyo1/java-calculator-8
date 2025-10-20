47d2c3955f3452bbb90419ae485b4b76e075fbbe
chyo1 <hyowon3856@gmail.com>
2025-10-20 16:49:52 +0900

    refactor: 빈 입력 확인 메서드 안 쓰는 매개변수 삭제

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -3,12 +3,12 @@ package calculator;
 public class Validator {
     // 입력이 형식에 맞게 들어왔는지 확인
     public void validateInputFormat(String s, char[] basicSeparators) {
-        validateInputEmpty(s, basicSeparators);
+        validateInputEmpty(s);
         validateSeparator(s, basicSeparators);
     }
 
     // 입력이 없을 때 (빈 문자열, null)
-    public void validateInputEmpty(String s, char[] basicSeparators) {
+    public void validateInputEmpty(String s) {
         if (s == null || s.isEmpty()) {
             throw new IllegalArgumentException("문자열이 입력되지 않았습니다");
         }
