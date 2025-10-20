187ceafdec579fbc6306fb9f19ec037fc4a65c50
chyo1 <hyowon3856@gmail.com>
2025-10-20 16:27:01 +0900

    feat: 커스텀 구분자 추출

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -1,8 +1,17 @@
 package calculator;
 
 public class Parser {
-    // 커스텀 구분자 유무 확인
+    private char[] basicSeperators = {',', ';'};
+    private Character customSeperator = null;
+    Validator validator = new Validator();
+
     // 커스텀 구분자 추출
+    public void checkCustomSeparator(String s) {
+        if (s.startsWith("//")) {
+            validator.validateCustomSeparatorForm(s);
+        }
+        customSeperator = s.charAt(2);
+    }
 
     // 구분자 기준 분리
 }
