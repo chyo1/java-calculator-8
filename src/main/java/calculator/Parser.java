30a261c01cbb8938143d457dcc07218fd480e77e
chyo1 <hyowon3856@gmail.com>
2025-10-20 17:06:55 +0900

    refactor: 커스텀 구분자 추출 메서드 Calculator로 이동

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -3,15 +3,6 @@ package calculator;
 public class Parser {
     Validator validator = new Validator();
 
-    // 커스텀 구분자 추출
-    public Character getCustomSeparator(String s) {
-        if (s.startsWith("//")) {
-            validator.validateCustomSeparatorForm(s);
-            return s.charAt(2);
-        }
-        return null;
-    }
-
     // 구분자 기준 분리
     public String[] getNumbers(String s, char[] basicSeparators, Character customSeparator) {
         StringBuilder sb = new StringBuilder();
