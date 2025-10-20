de534a5e75ea6010e7539704f5c6b99a5eff6811
chyo1 <hyowon3856@gmail.com>
2025-10-20 14:31:12 +0900

    feat: 입, 출력 메서드

--- src/main/java/calculator/IOController.java
+++ src/main/java/calculator/IOController.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class IOController {
     // 입력 받기
+    public String input() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
+    }
 
     // 출력하기
+    public void Print(int result) {
+        System.out.println("결과 : " + result);
+    }
 }
