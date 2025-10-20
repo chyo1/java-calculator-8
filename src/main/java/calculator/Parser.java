18d81600f79d0034c93846e6c662d9511ad57871
chyo1 <hyowon3856@gmail.com>
2025-10-20 17:00:59 +0900

    chore: typo

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -13,12 +13,12 @@ public class Parser {
     }
 
     // 구분자 기준 분리
-    public String[] getNumbers(String s, char[] basicSeperators, Character customSeperator) {
+    public String[] getNumbers(String s, char[] basicSeparators, Character customSeparator) {
         StringBuilder sb = new StringBuilder();
-        sb.append(basicSeperators[0] + '|');
-        sb.append(basicSeperators[1] + '|');
-        if (customSeperator != null) {
-            sb.append(customSeperator);
+        sb.append(basicSeparators[0] + '|');
+        sb.append(basicSeparators[1] + '|');
+        if (customSeparator != null) {
+            sb.append(customSeparator);
         }
 
         String separators = sb.toString();
