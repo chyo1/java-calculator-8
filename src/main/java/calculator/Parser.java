ee6c96c49110841d11bf1f1eb00f34bb25119033
chyo1 <hyowon3856@gmail.com>
2025-10-20 17:00:26 +0900

    refactor: 커스텀 구분자 추출 -> 반환으로 변경

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -4,11 +4,12 @@ public class Parser {
     Validator validator = new Validator();
 
     // 커스텀 구분자 추출
-    public void checkCustomSeparator(String s) {
+    public Character getCustomSeparator(String s) {
         if (s.startsWith("//")) {
             validator.validateCustomSeparatorForm(s);
+            return s.charAt(2);
         }
-        customSeperator = s.charAt(2);
+        return null;
     }
 
     // 구분자 기준 분리
