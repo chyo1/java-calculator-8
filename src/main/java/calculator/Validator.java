8fa7c16fa48aa4094e530e2465fe909a2a2b6ca9
chyo1 <hyowon3856@gmail.com>
2025-10-20 14:38:16 +0900

    feat: 기본 구분자 입력 에러 처리

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -14,7 +14,20 @@ public class Validator {
     }
 
     // 구분자가 형식에 맞게 입력됐는지 검증
+    public void validateSeparator(String s) {
+        validateBasicSeparator(s);
+    }
+
     // 기본 구분자가 형식에 맞게 입력됐는지
+    public void validateBasicSeparator(String s) {
+        String[] splitInput = s.split(",|;");
+        for (String split : splitInput) {
+            if (split.isEmpty()) {
+                throw new IllegalArgumentException("기본 구분자의 입력이 형식에 맞지 않습니다.");
+            }
+        }
+    }
+
     // 커스텀 구분자가 1개 이상 들어왔는지
     // 커스텀 구분자가 형식에 맞게 입력됐는지
     // 커스텀 구분자가 문자열 앞 부분에 위치해 있는지
