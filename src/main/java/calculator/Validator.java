9e79550df3c6cc93c5f351b0f2a45c887e6842f8
chyo1 <hyowon3856@gmail.com>
2025-10-20 14:34:40 +0900

    feat: 입력 에러 처리

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -2,7 +2,17 @@ package calculator;
 
 public class Validator {
     // 입력이 형식에 맞게 들어왔는지 확인
+    public void validateInputFormat(String s) {
+        validateInputEmpty(s);
+    }
+
     // 입력이 없을 때 (빈 문자열, null)
+    public void validateInputEmpty(String s) {
+        if (s == null || s.isEmpty()) {
+            throw new IllegalArgumentException("문자열이 입력되지 않았습니다");
+        }
+    }
+
     // 구분자가 형식에 맞게 입력됐는지 검증
     // 기본 구분자가 형식에 맞게 입력됐는지
     // 커스텀 구분자가 1개 이상 들어왔는지
